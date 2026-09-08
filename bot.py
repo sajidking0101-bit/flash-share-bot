703e7b868a859fecf5929f32a98233a7554cb876
sajidking0101-bit <sajidking0101@gmail.com>
2026-09-08 23:24:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,37 @@
+import os
+import telebot
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        "🔒 FILE LOCKED\n\n"
+        "Your file is ready!\n"
+        "🔓 Unlock the file to continue.",
+        reply_markup=unlock_button()
+    )
+
+def unlock_button():
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(
+        telebot.types.InlineKeyboardButton(
+            "🔓 UNLOCK FILE",
+            callback_data="unlock"
+        )
+    )
+    return markup
+
+@bot.callback_query_handler(func=lambda call: call.data == "unlock")
+def unlock(call):
+    bot.answer_callback_query(call.id)
+    bot.send_message(
+        call.message.chat.id,
+        "⏳ Ad/verification system abhi setup hona baaki hai."
+    )
+
+print("Bot is running...")
+bot.infinity_polling()
